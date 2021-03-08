78a2c904486355cfb8d053e03ea3e9b574ee3c7a
leiwenpeng <leiwenpeng1994@outlook.com>
2021-03-08 15:55:25 +0800

    feature(concepts):
    walk through basic concepts.

--- basic_concepts/src/main.rs
+++ basic_concepts/src/main.rs
@@ -1,3 +1,5 @@
+mod control_flow;
+
 fn main() {
     // rust声明的变量都是不可修改的，无法经行重新的赋值。
     let a = 12;
@@ -125,8 +127,76 @@ fn main() {
     // 相同的是，数组也是声明的时候固定长度，切无法动态地修改。
     let _f = [1, 2, 3, 4];
     // rust 还存在一个 Vector的类型，Vector 就是增强版的 Array，可变长，内部可以存放不同类型的元素。
-    // 数组声明的一个技巧, let list: [Type;Count] = [...], 
+    // 数组声明的一个技巧, let list: [Type;Count] = [...],
     // 可以用在类型注释中，同样可以用在声明的值， let list = [3; 5];
     let _f: [i32; 5] = [1, 2, 3, 4, 5];
     // 访问数组通过 list[index]的形式
+
+    println!("{}", output_number());
+    expression_example();
+    let x = expressions_example_2();
+    println!("{:?}", x);
+    println!("---------------------");
+    println!("{}", control_flow::control_flow::produce_num_five());
+    control_flow::control_flow::if_else(3);
+    control_flow::control_flow::let_if(true);
+    // control_flow::control_flow::loop_code();
+    control_flow::control_flow::while_code();
+    control_flow::control_flow::for_code();
+    println!("{:?}", control_flow::control_flow::convert_c_to_h(25.5));
+    println!("{:?}", control_flow::control_flow::fibonacci(10));
+}
+
+// 函数，通过fn关键字定义，函数名称后跟着圆括号，然后跟着一对大括号，标识着函数体的开始与结尾。
+// 定义完成之后，可以在main函数中调用。
+fn output_number() -> i32 {
+    23
+}
+
+// 函数参数，函数参数是一种特殊变量，是函数签名的一部分，
+// 当函数拥有参数时，可以为这些参数提供具体的值。
+// 多参数使用都好分割
+fn output_number_2(num1: i32, num2: i32) -> i32 {
+    num1 + num2
+}
+
+// 语句与表达式
+// Rust是一门基于表达式的语言，
+// 在其他的语言中，表达式和语句可能并没有什么区别，
+// 但是在Rust中理解表达式(expressions)和语句(statements)的区别是很重要的。
+// ---------------------
+// 语句，是执行一些操作，但是不返回值的指令。
+// 表达式，需要计算并参生一个值。
+fn expression_example() {
+    let _x = 6; // 语句，只声明了一个变量，不返回变量。
+                // let _y = _x = 6; // 该语句再Rust中回报错，但是在C，Ruby，JavaScript中都会成功，_y和_x都是6。
+
+    // 表达式会计算值。
+
+    let x = 5;
+
+    let y = {
+        let x = 3;
+        x + 1 // 表达式返回 x+1 的值，赋值给 y。
+    };
+
+    println!("{:?}", y);
+    println!("{:?}", x);
+}
+// 在Rust中，大部分编写的代码都将会是表达式，
+// 上面编写的语句 let _x = 6; 等式右边的六其实就是一个表达式，返回6，并赋值给右边声明的变量 _x。
+// 所以，表达式可以是语句的一部分。
+// 函数调用是一个表达式，宏调用是一个表达式，我们用于创建新作用域的大括号{}，也是一个表达式。
+// 注意上面函数体中的
+/*
+    let y = {
+        let x = 3;
+        x + 1 // 如果最后结尾加了分号，那它就不是表达式，而是一个语句。
+        // 这与与大多数语言的写法不同。加了分号就表示一个语句，不加就表示一个表达式。
+    }
+*/
+
+// 具有返回值的函数
+fn expressions_example_2() -> i8 {
+    5
 }
